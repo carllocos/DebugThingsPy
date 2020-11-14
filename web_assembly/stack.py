4e0dab196f9091b1a8b58c461a34000fd8ff4798
carllocos <carlosj.rojascastillo@gmail.com>
2020-11-14 22:24:14 +0100

    added naive stack build

---
+++ web_assembly/stack.py
@@ -0,0 +1,154 @@
+from enum import Enum
+
+
+class BreapPoint:
+
+    def __init__(self, module_name, line_number, byte_addr):
+        super().__init__()
+
+    def module_name(self):
+        return self.__module_name
+
+    def line_number(self):
+        return self.__line_number
+
+    def byte_addr(self):
+        return self.__byte_addr
+
+
+class ConstType(Enum):
+    I32 = 0
+    I64 = 1
+
+    @staticmethod
+    def from_str(type_str):
+        if len(type_str) == 0 or type_str[0] != 'i':
+            raise ValueError('incorrect str. Must be of the form `i32`')
+        _bits = int(type_str[1:], 10)
+        if _bits % 32 != 0 or _bits > 64:
+            raise ValueError('incorrect constant')
+
+        idx = int((_bits / 32) - 1)
+        types = [ConstType.I32, ConstType.I64]
+        return types[idx]
+
+
+class StackValues:
+
+    def __init__(self):
+        self.__values = []
+        self.__idx = None
+
+    def from_idx(self, idx):
+        if idx >= len(self.__values):
+            return False
+        return self.__values[idx]
+
+    def next_val(self):
+        if self.__idx is None or self.__idx >= len(self.__values):
+            self.__idx = None
+            return False
+        v = self.__values[self.__idx]
+        self.__idx += 1
+        return v
+
+    def reset_iterator(self):
+        self.__idx = 0
+        self.__values.sort(key = lambda sv: sv['idx'], reverse= True)
+
+    def add_value(self, type_str, val, idx):
+        v = StackValues.stack_value(type_str, val, idx)
+        self.__values.append(v)
+
+    def set_orignal(self, org):
+        self.original = org
+
+    @staticmethod
+    def stack_value(type_str, val, idx):
+        return {'type': ConstType.from_str(type_str), 'value': val, 'idx': idx}
+
+class BlockType(Enum):
+    FUNC = 0
+    INIT_EXP = 1
+    BLOCK = 2
+    LOOP = 3
+    IF = 4
+
+    @staticmethod
+    def from_int(i):
+        if not isinstance(i, int):
+            raise ValueError('integer expected')
+
+        types = [BlockType.FUNC, BlockType.INIT_EXP, BlockType.BLOCK, BlockType.LOOP, BlockType.IF]
+        if i >= len(types):
+            raise ValueError(f'incorrect BlockType idx {i}')
+        return types[i]
+
+class Frame:
+
+    def __init__(self, block_type, module, name, idx, fp, sp, ret_addr, func_id):
+        self.block_type = block_type
+        self.module = module
+        self.name = name
+        self.idx = idx
+        self.fp = fp
+        self.sp = sp
+        self.ret_addr = ret_addr
+        self.func_id =func_id
+
+    def as_dict(self):
+        d =  {'block_type': self.block_type,
+              'module': self.module,
+              'name': self.name,
+              'fidx': self.func_id,
+              'idx': self.idx,
+              'fp': self.fp,
+              'sp': self.sp,
+              'ret_addr': self.ret_addr}
+        return d
+
+    def __repr__(self):
+        return f'{self.as_dict()}'
+
+
+class CallStack:
+    def __init__(self, funcs):
+        self.__funcs = funcs
+        self.__idx = None
+        self.__frames = []
+
+    def next_frame(self):
+        if self.__idx is None or self.__idx >= len(self.__frames):
+            self.__idx = None
+            return False
+        f = self.__frames[self.__idx]
+        self.__idx += 1
+        return f
+
+    def from_idx(self, idx):
+        if idx >= len(self.__frames):
+            return False
+        return self.__frames[idx]
+
+    def reset_iterator(self):
+        self.__idx = 0
+        self.__frames.sort(key = lambda f: f.idx, reverse= True)
+
+    def add_frame(self, frame):
+        self.__frames.append(frame)
+
+    def set_orignal(self, org):
+        self.original = org
+
+    def funcs(self):
+        return self.__funcs
+
+def make_fun(module, name, func_id, from_addr, to_addr, args_amounts, locals_amount):
+    f = {'module': module,
+         'name': name,
+         'id': func_id,
+         'start_addr':from_addr,
+         'start_addr':from_addr,
+         'q_args': args_amounts,
+         'q_locals': locals_amount}
+    return f
--- web_assembly/wa.py
+++ web_assembly/wa.py
@@ -1,15 +1,68 @@
+from web_assembly import stack
+
+from utils import util
+
+raw_json =('0x5b', {'dump': {'pc': '0x3ffbec4b', 'start': ['0x3ffbebf0'], 'breakpoints': ['0x3ffbec4b'], 'functions': [{'fidx': '0x0', 'from': '0x3ffbec1d', 'to': '0x3ffbec1d'}, {'fidx': '0x1', 'from': '0x3ffbec20', 'to': '0x3ffbec3a'}, {'fidx': '0x2', 'from': '0x3ffbec3f', 'to': '0x3ffbec50'}], 'callstack': [{'type': 0, 'fidx': '0x2', 'sp': -1, 'fp': -1, 'ra': '0x0'}, {'type': 3, 'fidx': '0x0', 'sp': 0, 'fp': 0, 'ra': '0x3ffbec45'}]}, 'bp': '0x3ffbec4b'}, {'local_dump': {'stack': [{'idx': 0, 'type': 'i32', 'value': 5}, {'idx': 1, 'type': 'i32', 'value': 120}]}, 'bp': '0x3ffbec4b'})
+raw_dump = {'dump': {'pc': '0x3ffbec37', 'start': ['0x3ffbebf0'], 'breakpoints': ['0x3ffbec37'], 'functions': [{'fidx': '0x0', 'from': '0x3ffbec1d', 'to': '0x3ffbec1d', 'args': 1, 'locs': 0}, {'fidx': '0x1', 'from': '0x3ffbec20', 'to': '0x3ffbec3a', 'args': 2, 'locs': 0}, {'fidx': '0x2', 'from': '0x3ffbec3f', 'to': '0x3ffbec50', 'args': 0, 'locs': 1}], 'callstack': [{'type': 0, 'fidx': '0x2', 'sp': -1, 'fp': -1, 'ra': '0x0'}, {'type': 3, 'fidx': '0x0', 'sp': 0, 'fp': 0, 'ra': '0x3ffbec45'}, {'type': 0, 'fidx': '0x1', 'sp': 0, 'fp': 0, 'ra': '0x3ffbec4b'}, {'type': 4, 'fidx': '0x0', 'sp': 3, 'fp': 1, 'ra': '0x3ffbec27'}, {'type': 0, 'fidx': '0x1', 'sp': 2, 'fp': 1, 'ra': '0x3ffbec33'}, {'type': 4, 'fidx': '0x0', 'sp': 5, 'fp': 3, 'ra': '0x3ffbec27'}, {'type': 0, 'fidx': '0x1', 'sp': 4, 'fp': 3, 'ra': '0x3ffbec33'}, {'type': 4, 'fidx': '0x0', 'sp': 7, 'fp': 5, 'ra': '0x3ffbec27'}, {'type': 0, 'fidx': '0x1', 'sp': 6, 'fp': 5, 'ra': '0x3ffbec33'}, {'type': 4, 'fidx': '0x0', 'sp': 9, 'fp': 7, 'ra': '0x3ffbec27'}, {'type': 0, 'fidx': '0x1', 'sp': 8, 'fp': 7, 'ra': '0x3ffbec33'}, {'type': 4, 'fidx': '0x0', 'sp': 11, 'fp': 9, 'ra': '0x3ffbec27'}]}, 'bp': '0x3ffbec37'}
+raw_locals = {'local_dump': {'stack': [{'idx': 0, 'type': 'i32', 'value': 5}, {'idx': 1, 'type': 'i64', 'value': 13}, {'idx': 2, 'type': 'i32', 'value': 5}, {'idx': 3, 'type': 'i64', 'value': 14}, {'idx': 4, 'type': 'i32', 'value': 4}, {'idx': 5, 'type': 'i64', 'value': 15}, {'idx': 6, 'type': 'i32', 'value': 3}, {'idx': 7, 'type': 'i64', 'value': 16}, {'idx': 8, 'type': 'i32', 'value': 2}, {'idx': 9, 'type': 'i64', 'value': 17}, {'idx': 10, 'type': 'i32', 'value': 1}]}, 'bp':'0x3ffbec37 '}
+raw_complex = ('0x47', raw_dump, raw_locals)
 
 
-class BreapPoint:
 
-    def __init__(self, module_name, line_number, byte_addr):
-        super().__init__()
+def raw_to_stack(raw):
+    if not isinstance(raw, tuple):
+        raise ValueError('expects a tuple size 3 (bp, dump, locals)')
+    if len(raw) != 3:
+        raise ValueError('expects a tuple size 3 (bp, dump, locals)')
 
-    def module_name(self):
-        return self.__module_name
+    (_bp, _dump_dict, _locals) = raw
+    _dump = _dump_dict['dump']
+    _offset =  _dump['start'][0]
+    _funcs = [ to_fun(f, _offset) for f in _dump['functions']]
+    _cs = stack.CallStack(_funcs)
+    for idx, f in enumerate(_dump['callstack']):
+        _frame = to_frame(f, _offset, idx)
+        _cs.add_frame(_frame)
 
-    def line_number(self):
-        return self.__line_number
+    _cs.set_orignal(_dump)
+    _vals = _locals['local_dump']['stack']
+    _sv = stack.StackValues()
+    for v in _vals:
+        _sv.add_value(v['type'], v['value'], v['idx'])
 
-    def byte_addr(self):
-        return self.__byte_addr
+    _sv.set_orignal(_locals['local_dump'])
+    return (_bp, _cs, _sv)
+
+def to_frame(cs_json, offset, idx):
+    _type = stack.BlockType.from_int(cs_json['type'])
+    _func_id = cs_json['fidx'] if _type == stack.BlockType.FUNC else None
+    _module = 'some_mod'
+    _name = 'some_name'
+    _idx = idx
+    _fp = cs_json['fp'] if cs_json['fp'] != -1 else None
+    _sp = cs_json['sp'] if cs_json['sp'] != -1 else None
+    _retaddr = None
+    if cs_json['ra'] != '0x0':
+        _retaddr = util.substract_hexs([cs_json['ra'], offset])
+
+    return stack.Frame(_type,_module, _name, _idx, _fp, _sp, _retaddr, _func_id)
+
+
+def to_fun(fun_json, offset):
+    _start_addr = util.substract_hexs([fun_json['from'], offset])
+    _end_addr = util.substract_hexs([fun_json['to'], offset])
+    _name = 'some_name'
+    _module_name= 'module_name'
+    _id = fun_json['fidx']
+    _qargs = fun_json['args']
+    _qlocals = fun_json['locs']
+    return stack.make_fun(_module_name, _name, _id, _start_addr, _end_addr, _qargs, _qlocals)
+
+def do_complex():
+    return raw_to_stack(raw_complex)
+
+def do_test():
+    return raw_to_stack(raw_json)
+
+
+
