213fd980005e53c2f15c27902f877147209c37e6
carllocos <carlosj.rojascastillo@gmail.com>
2021-05-21 11:41:55 +0200

    correct debugsession time bench

--- dbg.py
+++ dbg.py
@@ -65,13 +65,83 @@ def load_config(path: Union[str, None] = None)-> None:
     return config
 
 
+def space_needed(arg):
+    frames = 2 * (arg + 1) + 2
+    vals = arg + 1
+    default = 0x100
 
-def benchmark_fac_limits(arg):
+    callstack_size = default
+    stack_size = default
+
+    counter1 = 0
+    counter2 = 0
+    while callstack_size < frames:
+        counter1 += 1
+        callstack_size += 256
+
+    while stack_size < vals:
+        counter2 += 1
+        stack_size += 256
+    obj = {
+        'callstack_size_below': hex(callstack_size - 256),
+        'cs_increase': counter1 - 1,
+        'stack_size_below': hex(stack_size - 256),
+        'stack_increase': counter2 - 1,
+    }
+    print(obj)
+
+    return {
+        'frames': frames,
+        'stack': vals,
+        'callstack_size': hex(callstack_size),
+        'cs_increase': counter1,
+        'stack_size': hex(stack_size),
+        'stack_increase': counter2,
+    }
+def accurate_size(arg):
+    frames = 2 * (arg + 1) + 2
+    vals = arg + 1
+    return {
+        'frames': frames,
+        'frames_hex' : hex(frames),
+        'stack': vals,
+        'stack_hex': hex(vals)
+    }
+
+def benchmark_sizes(arg):
     import time
-    [i] = mod.linenr(30)
 
     dev = rmt
-    dev.bench_name("double_bench_fac_" +str(arg) + ".txt")
+    dev.connect()
+    [i] = mod.linenr(28)
+    dev.add_breakpoint(i)
+    dev.run()
+
+    while dev.session is None:
+        print("sleeping 0.5secs")
+        time.sleep(0.5)
+
+    outputfile = 'decr_debugsession_sizes.txt'
+    callstack_size = 2 * (arg + 1) + 2
+    f = open(outputfile , "a")
+    f.write(f'arg={arg},callstack={callstack_size},session_size={dev.session.total_size}\n')
+    f.close()
+    
+def benchmark_decr_limits(arg, x = None, y = None ):
+    import time
+    output_name = ''
+    if  x is None:
+        output_name = f"stickc_decr_"+ str(arg) + ".txt"
+    else:
+        if y is None:
+            output_name = f"stickc_decr_csx{x}_"+ str(arg) + ".txt"
+        else:
+            output_name = f"stickc_decr_csx{x}_sx{y}_"+ str(arg) + ".txt"
+
+    [i] = mod.linenr(28)
+
+    dev = rmt
+    dev.bench_name(output_name)
     dev.connect()
     dev.add_breakpoint(i)
     dev.run()
@@ -85,7 +155,13 @@ def benchmark_fac_limits(arg):
         if i != 9:
             print("Send Run")
             dev.run()
-    print("done bechmark_FAC_LIMITS")
+
+    expected_frames = 2 * (arg + 1) + 2
+    l = len(dev.session.callstack.all_frames)
+    reached = "" if expected_frames == l else f"not reached. Got {l}"
+    print(f"done bechmark_FAC_LIMITS file={output_name}, Expected frames={expected_frames} {reached}")
+    if dev.session.exception:
+        print(f"exception occurred: {dev.session.exception}")
 
 if __name__ == "__main__":
     path = None
