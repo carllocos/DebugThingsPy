2a7cacd1a042cff76c639df825d83eae8c817e25
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2022-04-20 16:02:39 +0200

    Disable logging to stderr

--- warduino/mylogger.py
+++ warduino/mylogger.py
@@ -1,18 +1,19 @@
 import sys
 import inspect
 
-DEBUG = True
+DEBUG = False
+
 
 def stderr_print(s):
     globals, DEBUG
 
     curframe = inspect.currentframe()
     calframe = inspect.getouterframes(curframe, 2)
-    cn =str(calframe[1][3])
-    if DEBUG:# and cn in ONLY:
-        print((cn + ':').upper(), s, file = sys.stderr)
+    cn = str(calframe[1][3])
+    if DEBUG:  # and cn in ONLY:
+        print((cn + ':').upper(), s, file=sys.stderr)
+
 
 def errprint(s):
     print(s)
     sys.exit()
-
