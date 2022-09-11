001739dd3116ae4d922ad17b6b7968641521e922
Carlos Rojas <carlosj.rojas.castillo@gmail.com>
2022-09-11 11:17:37 +0200

    merge fix

--- warduino/mylogger.py
+++ warduino/mylogger.py
@@ -1,5 +1,6 @@
 import sys
 import inspect
+import logging
 
 DEBUG = False
 
@@ -11,9 +12,19 @@ def stderr_print(s):
     calframe = inspect.getouterframes(curframe, 2)
     cn = str(calframe[1][3])
     if DEBUG:  # and cn in ONLY:
-        print((cn + ':').upper(), s, file=sys.stderr)
+        print((cn + ":").upper(), s, file=sys.stderr)
 
 
 def errprint(s):
     print(s)
     sys.exit()
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    c_handler = logging.StreamHandler()
+    c_handler.setFormatter(logging.Formatter("[%(levelname)s] %(name)s - %(message)s"))
+    logger.addHandler(c_handler)
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    return logger
